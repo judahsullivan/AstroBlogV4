0755c10c4920207d597ba58a5bcac23e5457d608
judahsullivan <judahsullivan.dev@gmail.com>
2024-05-21 22:10:06 -0500

    working on the final parts of the front end

--- src/scripts/app/Home/hero.ts
+++ src/scripts/app/Home/hero.ts
@@ -2,45 +2,61 @@ import gsap from "gsap";
 
 export function HeroAnimations() {
   const heroSections = document.querySelectorAll(".hero");
+  const herotl = gsap.timeline();
 
   heroSections.forEach((heroSection) => {
     const title = heroSection.querySelector(".title");
     const desc = heroSection.querySelector(".desc");
+    const cards = heroSection.querySelectorAll(".link-card");
     const imgContainer = heroSection.querySelector(".img-container");
     const img = heroSection.querySelector(".hero-img");
 
     // Set initial state of imgContainer to cover the entire screen
-    gsap.set(imgContainer, {
+    herotl.set(imgContainer, {
       y: "-60%",
       borderRadius: "0px",
       scale: 1.07,
     });
 
-    gsap.to(imgContainer, {
-      delay: 1,
-      y: 50,
-      x: -50,
-      duration: 1.5,
-      position: "initial",
-      borderRadius: "15px",
-      scale: 0.95,
-      ease: "power3.out",
-    });
+    herotl
+      .to(imgContainer, {
+        delay: 1,
+        y: 50,
+        x: -50,
+        duration: 1.5,
+        position: "initial",
+        borderRadius: "15px",
+        scale: 0.9,
+        ease: "power3.out",
+      })
+      .fromTo(
+        [title, desc],
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          duration: .5,
+          delay: .3,
+          stagger: 0.09,
+          ease: "power4.inOut",
+        }
+      );
+    if (cards) {
+      herotl.fromTo(
+        cards,
+        {
+          scale: 0,
+        },
+        {
+          scale: 1,
+          duration: 0.5,
+          stagger: 0.3,
+        }
+      );
+    }
 
-    gsap.fromTo(
-      [title, desc],
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        duration: 1,
-        delay: 1.5,
-        stagger: 0.09,
-        ease: "power4.inOut",
-      }
-    );
-    gsap.from(img, {
+    herotl.from(img, {
       scale: 1.05,
       delay: 1.2,
       duration: 1.2,
