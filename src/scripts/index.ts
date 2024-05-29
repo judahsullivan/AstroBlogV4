7cd17759f026c0c991f5d0d1ea48c0c683aef9b5
judahsullivan <judahsullivan.dev@gmail.com>
2024-05-29 00:45:43 -0500

    working on the stuck animation

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,33 +4,27 @@ import { LayoutEnter, LayoutExit } from "./layout";
 import { homeView, blogView, articleViews } from "./views";
 import { InitScroll } from "./Locomotive";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const initializeBarba = async () => {
-    const articleViewList = await articleViews();
+const articleViewList = await articleViews();
 
-    barba.init({
-      views: [homeView, blogView, ...articleViewList],
-      transitions: [
-        {
-          name: "default",
-          once({ next }) {
-            NavigationAnimation();
-            LayoutEnter(next.container);
-          },
-          leave({ current }) {
-            return LayoutExit(current.container); // Ensure this returns Promise<void>
-          },
-          enter({ next }) {
-            LayoutEnter(next.container);
-          },
-        },
-      ],
-    });
+barba.init({
+  views: [homeView, blogView, ...articleViewList],
+  transitions: [
+    {
+      name: "default",
+      once({ next }) {
+        NavigationAnimation();
+        LayoutEnter(next.container);
+      },
+      leave({ current }) {
+        return LayoutExit(current.container); // Ensure this returns Promise<void>
+      },
+      enter({ next }) {
+        LayoutEnter(next.container);
+      },
+    },
+  ],
+});
 
-    barba.hooks.after(() => {
-      InitScroll();
-    });
-  };
-
-  initializeBarba();
+barba.hooks.after(() => {
+  InitScroll();
 });
